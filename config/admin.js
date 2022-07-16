79b5d15b974faf218160ad1d933efdd67d513b01
zlatkoflash <zlatkoflash@gmail.com>
2022-07-16 18:40:51 +0200

    Add files via upload

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    /*secret: env('ADMIN_JWT_SECRET'),*/
+    secret: 'secret_token_xasdasd',
   },
   apiToken: {
     /*salt: env('API_TOKEN_SALT'),*/
-    salt: 'sto_je_ova'
+    salt: 'salt_token_xasdasd'
   },
 });
